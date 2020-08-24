fb1b2032aa6e7f96f6fc921e86c343a48cf6f815
mikbry <mik@miklabs.com>
2020-08-24 20:05:07 +0200

    feat test json decodable

--- MacPlayground.playground/Contents.swift
+++ MacPlayground.playground/Contents.swift
@@ -1,7 +1,20 @@
 import Cocoa
 
+struct Input: Decodable {
+    let text: String
+    enum CodingKeys: String, CodingKey {
+        case text
+    }
+    init(from decoder: Decoder) throws {
+        let singleValue = try! decoder.singleValueContainer()
+        print(singleValue)
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        self.text = try values.decode(String.self, forKey: .text)
+    }
+}
+
 struct Intent: Decodable {
-    let input: String
+    let input: Array<Input>
     let output: String
 }
 
